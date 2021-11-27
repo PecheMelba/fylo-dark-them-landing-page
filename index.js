c833c3caf74914e94a0ed82f63bd0f34a283af46
Kevin Luccio <you@example.com>
2021-11-27 10:22:40 +0100

    corrected testimonials img

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
-console.log(document.getElementById("get-started-free-btn"));
 document.getElementById("get-started-free-btn").addEventListener('click', function()
-{
-    console.log("yyyyy");
+{    
         //if mail is correct format
         if(document.getElementById("email-input").validity.valid > 0)
         {            
